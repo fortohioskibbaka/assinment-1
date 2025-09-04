22fb114fc20317c3c1c1a1538bc03e4441847872
339720419 <339720419@2260I-L29-001>
2025-09-04 09:38:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace assinment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fristName = "John";
+            string favmovie = "MATRIX";
+
+
+
+            Console.WriteLine($"my name is {fristName.ToLower()} and my fav movie is {favmovie.ToLower()}");
+            Console.WriteLine(favmovie.Contains("the"));
+        }
+    }
+}
