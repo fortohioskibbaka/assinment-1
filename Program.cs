c763ce498318d349bc35d4273d1da9916bc352eb
339720419 <339720419@2260I-L29-001>
2025-09-08 08:55:35 -0400

    done

--- Program.cs
+++ Program.cs
@@ -6,11 +6,42 @@
         {
             string fristName = "John";
             string favmovie = "MATRIX";
+            string favquote = "you need guys like me";
 
 
 
             Console.WriteLine($"my name is {fristName.ToLower()} and my fav movie is {favmovie.ToLower()}");
             Console.WriteLine(favmovie.Contains("the"));
+
+            // Replace all vowels in favquote with '*'
+            
+            favquote=(favquote.Replace("a", " "));
+            favquote = (favquote.Replace("i", " "));
+            favquote = (favquote.Replace("e", " "));
+            favquote = (favquote.Replace("o", " "));
+            favquote = (favquote.Replace("u", " "));
+            Console.WriteLine(favquote.Replace("a'e'i'o'u ", " "));
+            Console.WriteLine(favquote);
+
+           
+            
+           
+            Console.WriteLine("         .';:;'.");
+            Console.WriteLine("        /_' _' /\\   __");
+            Console.WriteLine("        ;a/ e= J/-'\"  '.'");
+            Console.WriteLine("        \\ ~_   (  -'  ( ;_ ,.");
+            Console.Write("         L~\"'_.    -.  \\ ./  )");       Console.WriteLine("            />_________________________________");
+            Console.Write("         ,'-' '-._  _;  )'   (");         Console.WriteLine("    [########[]_________________________________>");
+            Console.Write("       .' .'   _.'\")  \\  \\(  |");      Console.WriteLine("          ___");
+            Console.WriteLine("      /  (  .-'   __\\{`', \\  |");
+            Console.WriteLine("     / .'  /  _.-'   \"  ; /  |");
+            Console.WriteLine("    / /    '-._'-,     / / \\ (");
+            Console.WriteLine(" __/ (_    ,;' .-'    / /  /_'-._");
+            Console.WriteLine("`\"-'` ~`  ccc.'   __.','     \\j\\L\\");
+            Console.WriteLine("                 .=\'/|\\7      ");
+            Console.WriteLine("     snd           ' `");
+
         }
+       
     }
 }
